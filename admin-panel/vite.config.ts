61fd21aa06bebfe6059efa8312bc48e46b56f5b4
alexfun45 <alexfun45@gmail.com>
2026-01-20 19:37:41 +0300

    Изменены настройки vite.config.ts для доступа извне

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Важно! Для доступа снаружи
+    port: 5173,
+  },
   plugins: [
     react(),
     tailwindcss(),
