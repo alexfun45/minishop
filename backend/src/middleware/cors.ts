0e6195fdc35d7a0f8b0bba45e816e896f20f7745
alexfun45 <alexfun45@gmail.com>
2026-05-15 19:20:54 +0300

    добавлен origin в cors

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -4,10 +4,12 @@ const corsOptions = {
   origin: [
     'http://localhost:3001',
     'http://localhost:5173',
-    'http://87.120.166.203:5173'
+    'http://87.120.166.203:5173',
+    'http://testshop.alex-devlab.ru',
     //'http://admin.yourdomain.com', // Production admin
     //'https://admin.yourdomain.com'
   ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 };
 
